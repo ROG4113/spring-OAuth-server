4bcf3add31aa4ca8e2bb247d2e9f5e24b9854d82
ROG4113 <as0009916@gmail.com>
2026-05-05 15:52:08 +0530

    initial project

---
+++ src/main/java/com/aniket/springOAuthServer/SpringOAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.aniket.springOAuthServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOAuthServerApplication.class, args);
+	}
+
+}
